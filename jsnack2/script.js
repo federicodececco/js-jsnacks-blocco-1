88cca1bb84aa747b7031abcb84eee818fe22059a
federicodececco <federicodececco1@gmail.com>
2024-12-03 12:16:31 +0100

    add:message

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -8,9 +8,15 @@ let invites = [
     'Luca'
 ]
 let io = prompt("Nome")
+let flag = 0;
 for (let i = 0; i < invites.length; i++) {
 
     if (io === invites[i]) {
         console.log("Entra pure")
+        flag = 1
     }
+
+}
+if (flag === 0) {
+    console.log("Non puoi entrare")
 }
